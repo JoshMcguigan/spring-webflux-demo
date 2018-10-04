1187ef7502246ef5bd9e76be6fed92bcac6839f3
Josh Mcguigan <joshmcg88@gmail.com>
2018-10-04 15:27:11 -0700

    added blocking IO spring mvc endpoints

--- src/main/java/com/joshmcguigan/springwebfluxdemo/DemoController.java
+++ src/main/java/com/joshmcguigan/springwebfluxdemo/DemoController.java
@@ -17,7 +17,129 @@ public class DemoController {
     @GetMapping("/greeting")
     @ResponseBody
     public String greeting() {
-        return "hello";
+        return "greetings";
+    }
+
+    @GetMapping("/single")
+    @ResponseBody
+    public HttpBinResponse single() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpBinResponse response = restTemplate.getForObject("https://httpbin.org/delay/2", HttpBinResponse.class);
+
+        return response;
+    }
+
+    @GetMapping("/serial")
+    @ResponseBody
+    public List<HttpBinResponse> serial() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<HttpBinResponse> responses = new LinkedList<>();
+
+        for (int i = 1; i <= 3; i++) {
+            String url = String.format("https://httpbin.org/delay/%d", i);
+
+            HttpBinResponse response = restTemplate.getForObject(url, HttpBinResponse.class);
+            responses.add(response);
+        }
+
+
+        return responses;
+    }
+
+    @GetMapping("/parallel")
+    @ResponseBody
+    public List<HttpBinResponse> parallel() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<String> urls = new LinkedList<>();
+
+        for (int i = 1; i <= 3; i++) {
+            String url = String.format("https://httpbin.org/delay/%d", i);
+            urls.add(url);
+        }
+
+        List<HttpBinResponse> responses = urls.parallelStream() // this could be stream() instead of parallel stream
+                .map(url -> restTemplate.getForObject(url, HttpBinResponse.class))
+                .collect(Collectors.toList());
+
+        return responses;
+    }
+
+    @GetMapping("/parallel-many")
+    @ResponseBody
+    public List<HttpBinResponse> parallelMany() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<String> urls = new LinkedList<>();
+
+        // more urls than I have cores
+        for (int i = 1; i <= 3; i++) {
+            String url = String.format("https://httpbin.org/delay/%d", i);
+            urls.add(url);
+            urls.add(url);
+            urls.add(url);
+            urls.add(url);
+        }
+
+        List<HttpBinResponse> responses = urls.parallelStream()
+                .map(url -> restTemplate.getForObject(url, HttpBinResponse.class))
+                .collect(Collectors.toList());
+
+        return responses;
+    }
+
+    @GetMapping("/parallel-custom")
+    @ResponseBody
+    public List<HttpBinResponse> parallelCustom() throws ExecutionException, InterruptedException {
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<String> urls = new LinkedList<>();
+
+        // more urls than I have cores
+        for (int i = 1; i <= 3; i++) {
+            String url = String.format("https://httpbin.org/delay/%d", i);
+            urls.add(url);
+            urls.add(url);
+            urls.add(url);
+            urls.add(url);
+        }
+
+        ForkJoinPool forkJoinPool = new ForkJoinPool(12);
+        List<HttpBinResponse> responses = forkJoinPool.submit(() ->
+                urls.parallelStream()
+                        .map(url -> restTemplate.getForObject(url, HttpBinResponse.class))
+                        .collect(Collectors.toList())
+        ).get();
+
+        return responses;
+    }
+
+    @GetMapping("/parallel-overload")
+    @ResponseBody
+    public List<HttpBinResponse> parallelOverload() throws ExecutionException, InterruptedException {
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<String> urls = new LinkedList<>();
+
+        // more urls than I have cores
+        for (int i = 1; i <= 3; i++) {
+            String url = String.format("https://httpbin.org/delay/%d", i);
+            for (int j = 0; j < 50; j++) {
+                urls.add(url);
+            }
+        }
+
+        ForkJoinPool forkJoinPool = new ForkJoinPool(150);
+        List<HttpBinResponse> responses = forkJoinPool.submit(() ->
+                urls.parallelStream()
+                        .map(url -> restTemplate.getForObject(url, HttpBinResponse.class))
+                        .collect(Collectors.toList())
+        ).get();
+
+
+        return responses;
     }
 
 }
